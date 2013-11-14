d0160d7a400abe3f57926302bea8d5fa3f0b9bdb
Jon Galloway <jongalloway@gmail.com>
2013-11-13 18:13:57 -0800

    Initial commit

---
+++ WebApplication27/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication27.Startup))]
+namespace WebApplication27
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
